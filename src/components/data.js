3d3d759193fab6ca77f1856382b6dea19fc9ccc0
suryansh-vs <suryanshvk04@gmail.com>
2026-01-01 20:59:54 +0530

    Create data.js

---
+++ src/components/data.js
@@ -0,0 +1,122 @@
+export const list = [
+  {
+    id: 1,
+    name: "Palm Harbor",
+    location: "London",
+    price: "$2,095",
+    beds:"3",
+    bathrooms:"2",
+    area:"5*7 m2",
+    property_type:"Buy"
+  },
+  {
+    id: 2,
+    name: "Beverly Springheld",
+    location: "India",
+    price: "$2,700",
+    beds:"4",
+    bathrooms:"2",
+    area:"6*7.5 m2",
+    property_type:"Sell"
+  },
+  {
+    id: 3,
+    name: "Faulker Ave",
+    location: "Canada",
+    price: "$4,860",
+    beds:"4",
+    bathrooms:"3",
+    area:"8*10 m2",
+    property_type:"Rent"
+  },
+  {
+    id: 4,
+    name: "Jeringbone fausy",
+    location: "London",
+    price: "$7,540",
+    beds:"3",
+    bathrooms:"2",
+    area:"5*9 m2",
+    property_type:"commercial"
+  },
+  {
+    id: 5,
+    name: "Lane Realty",
+    location: "New York",
+    price: "$4,850",
+    beds:"6",
+    bathrooms:"2",
+    area:"9*7 m2",
+    property_type:"Sell"
+  },
+  {
+    id: 6,
+    name: "Banyon Tree Realty",
+    location: "Mexico",
+    price: "$3,780",
+    beds:"7",
+    bathrooms:"4",
+    area:"8*3 m2",
+    property_type:"Buy"
+  },
+  {
+    id: 7,
+    name: "Banyon Tree Realty",
+    location: "India",
+    price: "$3,780",
+    beds:"7",
+    bathrooms:"4",
+    area:"8*3 m2",
+    property_type:"Commercial"
+  },
+  {
+    id: 8,
+    name: "Banyon Tree Realty",
+    location: "Canada",
+    price: "$3,780",
+    beds:"7",
+    bathrooms:"4",
+    area:"8*3 m2",
+    property_type:"Sell"
+  },
+  {
+    id: 9,
+    name: "Banyon Tree Realty",
+    location: "USA",
+    price: "$3,780",
+    beds:"7",
+    bathrooms:"4",
+    area:"8*3 m2",
+    property_type:"Buy"
+  },
+  {
+    id: 10,
+    name: "Royol Exprise",
+    location: "London",
+    price: "$2,580",
+    beds:"7",
+    bathrooms:"4",
+    area:"8*3 m2",
+    property_type:"Sell"
+  },
+  {
+    id: 11,
+    name: "Jefry Nire",
+    location: "Canada",
+    price: "$2,800",
+    beds:"7",
+    bathrooms:"4",
+    area:"8*3 m2",
+    property_type:"Rent"
+  },
+  {
+    id: 12,
+    name: "Balcone",
+    location: "London",
+    price: "$2,780",
+    beds:"7",
+    bathrooms:"4",
+    area:"8*3 m2",
+    property_type:"Buy"
+  },
+]
