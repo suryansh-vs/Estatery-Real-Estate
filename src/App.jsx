a766355c159c2babcec15084d8584b9dcee571c3
suryansh-vs <suryanshvk04@gmail.com>
2026-01-01 20:58:06 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/navbar/header'
+import Main from './components/main/main'
+import Front from './components/front_page'
+
+function App() {
+  return (
+   <>
+    <Front/>
+    <Main/>
+    
+   </>
+  )
+}
+
+export default App
