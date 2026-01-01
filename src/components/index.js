3e8c0bc3fdd12dfb90b41df5483f12575a9c5e77
suryansh-vs <suryanshvk04@gmail.com>
2026-01-01 21:00:16 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+import './style.css'
+import logo from '../../assets/logo.png'
+
+function Content() {
+  return (
+    <>
+
+      <div className='modem'>
+        MODERN SERVICED APARTMENTS: A PLACE TO CALL YOUR OWN — FOR HOWEVER LONG YOU STAY
+      </div>
+    </>
+  )
+}
+
+export default Content
